d048272bc55572ba65e8626f037a3d841ff85f98
jobayed24 <jkhouse117@gmail.com>
2022-12-18 22:46:25 +0600

    array is a room where actually get value through via it's own index //// but iterator does't have index system but it's has own property to access it's values

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 const array1 = ['a', 'b', 'c','a'];
-console.log(typeof(array1));
 
-const iteator1=array1[Symbol.iterator]();
-console.log(iteator1);
-for(const newarray of array1){
-    console.log(newarray);
-}
-console.log("\n")
 
-for(const newanother of iteator1){
-    console.log(newanother);
-}
+
+const newtakeval= array1.values();
+
+console.log(newtakeval);
