c7b51c9183d8c3b0ec34165f103c594af3dc99f1
jobayed24 <jkhouse117@gmail.com>
2023-01-03 23:16:08 +0600

    array definition use case

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-///generator function
+// if you have list of care name and you store the data with seperate single variable 
+// then you will do it 
 
-const gn=function* (i){
-yield i;
-yield i+10;
-};
+const car1="volvo";
+const car2="saab";
+const car3="BMW";
+
+/// it's easy to print the specific value but seems you have 300 value then what happend you should use an array array is the varibale where you can store hug value doing seperate throught comma
 
 
-const gnend=gn(10);
-console.log(gnend.next().value);
-console.log(gnend.next().value);
+// infortent things is array have squre brackets where you put all value with make theme seperate
+const car=["volvo","saab","BMW"];
+
+// you can find the specific value through reference index number
+
+console.log(car[2]);
+
+
+// it's common practice to declare array with const keyword
