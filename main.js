c9cd40bcfcd7b5686535550b85e0ef037b988968
jobayed24 <jkhouse117@gmail.com>
2023-01-13 07:36:35 +0600

    organize array elements with ascending and also descending format with array sort() and reverse() method

--- main.js
+++ main.js
@@ -3,10 +3,27 @@ const randNumber=[
    4,
    2,
    10,
+   11,
+   15,
+   28
 ];
 
 
+ascenRandomNumber=randNumber.sort();
 
 
-document.getElementById("demo").innerHTML=randNumber.sort();
+
+
+
+
+
+
+doc1=document.getElementById("demo").innerHTML=ascenRandomNumber;
+console.log(doc1)
+
+doc2=document.getElementById("demo2").innerHTML=randNumber;
+
+console.log(doc2);
+
+
 
