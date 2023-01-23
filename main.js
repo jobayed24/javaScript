149c223871922690600402ef7aded12824f0c979
jobayed24 <jkhouse117@gmail.com>
2023-01-23 11:03:56 +0600

    findout the P tag which available in div1 tag

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 
-function getAllParaElems(){
-  const allPara=document.getElementsByTagName("p");
+///show all p in div1 
 
-  const allParaLenght=allPara.length;
 
-   alert(`There are ${allParaLenght} Paragraph it's mean p tag`)
+function div1ParaElems(){
+   const div1=document.getElementById("div1");
+   const allParaDiv = div1.getElementsByTagName("p");
+
+   let div1Para=allParaDiv.length;
+
+   alert(`There are ${div1Para} p tag available`);
 }
-
-
