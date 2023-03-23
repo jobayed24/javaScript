9bedf6995d020b3af5f1baeb878a72e48593938c
jobayed24 <jkhouse117@gmail.com>
2023-03-23 16:41:11 +0600

    encode and decode base 64 string by btoa and atob function

--- addEventListner.js
+++ addEventListner.js
@@ -4,3 +4,11 @@ function myFunction(){
     document.getElementById("demo").innerHTML="my name is Jobayed";
 
 }
+
+let value= document.getElementById("demo").innerText;
+
+let valueEncrypt=btoa(value);
+
+let valueDecrypt=atob(valueEncrypt);
+
+window.alert(valueDecrypt);
