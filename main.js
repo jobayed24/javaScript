38d4beb6d0306d74f1ebf3ca06d822da9943676d
jobayed24 <jkhouse117@gmail.com>
2023-06-18 01:48:31 +0600

    argument pass with parameter or missing paremeter value

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-/// default function constructor to defind the javascript functioin
+/// before es6 
 
-const myFunction=new Function("a","b","return a*b");
+function sum(a,b){
+   if(b===undefined){
+      b=4;
+   }
+
+   const x=a+b;
+   return x;
+}
 
 
-document.getElementById("demo").innerHTML=myFunction(4,5);
+document.getElementById("demo").innerHTML=sum(10);
+
+
+
+
+/// afeter es6
+
+function sum2(a,b=5){
+   const newSum=a+b;
+
+   return newSum;
+}
+
+document.getElementById("demo2").innerHTML=sum2(5);
