a484a4993d9e4377742e31bace07b839d0c6a610
jobayed24 <jkhouse117@gmail.com>
2023-06-20 15:19:04 +0600

    before es6 arrow funtion

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-class Car {
-   constructor(name) {
-     this.brand = name;
-   }
- 
-   present() {
-     return 'I have a ' + this.brand;
-   }
- }
- 
- class Model extends Car {
-   constructor(name, mod) {
-     super(name);
-     this.model = mod;
-   }  
-   show() {
-     return this.present() + ', it is a ' + this.model;
-   }
- }
- 
- const mycar = new Model("Ford", "Mustang");
+//it was the syntax before ES6;
 
- console.log(mycar.show());
+jobayed=function(){
+   console.log("my name is React JS");
+}
+
+
+jobayed();
